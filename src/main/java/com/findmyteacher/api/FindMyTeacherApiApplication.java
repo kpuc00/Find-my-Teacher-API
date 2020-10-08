dcf6b3a686e6a670a9d0549097bd9e0182974b52
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-10-08 13:04:30 +0200

    Initial commit

---
+++ src/main/java/com/findmyteacher/api/FindMyTeacherApiApplication.java
@@ -0,0 +1,13 @@
+package com.findmyteacher.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyTeacherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMyTeacherApiApplication.class, args);
+    }
+
+}
