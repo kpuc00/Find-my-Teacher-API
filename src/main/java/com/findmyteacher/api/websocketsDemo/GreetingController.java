0977dfe31078747114b01f16f0debbf91ffa3609
michael_gvdw <michael@fragrant-chios.com>
2020-12-16 12:12:31 +0100

    websocket azure funtionality

--- src/main/java/com/findmyteacher/api/websocketsDemo/GreetingController.java
+++ src/main/java/com/findmyteacher/api/websocketsDemo/GreetingController.java
@@ -3,7 +3,9 @@ package com.findmyteacher.api.websocketsDemo;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Controller
 public class GreetingController {
 
